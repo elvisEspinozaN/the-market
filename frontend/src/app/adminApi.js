a41558c380bb1965e8429c92a3438b33b15e1870
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-05-15 21:04:34 -0400

    implement adminApi slice for admin functionality

---
+++ frontend/src/app/adminApi.js
@@ -0,0 +1,60 @@
+import apiSlice from "./apiSlice";
+
+export const adminApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => "/admin/users",
+      providesTags: ["Users"],
+    }),
+
+    promoteUser: builder.mutation({
+      query: (userId) => ({
+        url: `/admin/users/${userId}/make-admin`,
+        method: "POST",
+      }),
+      invalidatesTags: ["Users"],
+    }),
+
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `/admin/users/${userId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Users"],
+    }),
+
+    createProduct: builder.mutation({
+      query: (body) => ({
+        url: "/admin/products",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Products"],
+    }),
+
+    updateProduct: builder.mutation({
+      query: ({ id, ...body }) => ({
+        url: `/admin/products/${id}`,
+        method: "POST",
+        body,
+      }),
+    }),
+
+    deleteProduct: builder.mutation({
+      query: (id) => ({
+        url: `/admin/products/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Products"],
+    }),
+  }),
+});
+
+export const {
+  useGetUsersQuery,
+  usePromoteUserMutation,
+  useDeleteUserMutation,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useDeleteProductMutation,
+} = adminApi;
